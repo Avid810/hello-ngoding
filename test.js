a2afc5899989bf12c712a36537cffb0eb46a679a
Avid <ar.tech.grafis@gmail.com>
2023-08-08 15:26:28 +0700

    fist commit

---
+++ test.js
@@ -0,0 +1,19 @@
+let number1 = '8';
+let number2 = '2';
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+
+// const hewan = 'jerapah';
+
+const hewan = 'gajah'
+console.log(hewan)
+
+let number4 = '2';
+let ket = 'ekor';
+let hewan2 = 'Jerapah';
+
+
+console.log(number4 + ' ' + ket + ' ' + hewan2);
